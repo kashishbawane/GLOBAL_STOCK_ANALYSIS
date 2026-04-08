e6d6ef7404169dff0889e43507fa8404d5198c97
Kashish Jayant Bawane <kashishbawane17@gmail.com>
2026-04-08 18:02:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 st.set_page_config(page_title="Global Income Inequality", layout="wide")
 
